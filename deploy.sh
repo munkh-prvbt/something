f4d26bc8ea6f13f596174b92553d2d9c26645c3d
Munkh <munkh.purevbat@erin.systems>
2022-03-29 02:02:33 +0800

    fix: deployment

--- deploy.sh
+++ deploy.sh
@@ -15,4 +15,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
+git push -f git@github.com:munkh-prvbt/somethings.git main:gh-pages
+
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/" + process.env.PROJECT_NAME + "/"
-      : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/somethings/" : "/",
 };
